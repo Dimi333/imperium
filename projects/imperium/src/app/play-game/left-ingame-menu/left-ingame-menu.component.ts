f3a43f144e14a58a08f0cda7a49304c3a54b8ece
dimi3 <oresansky@gmail.com>
2020-09-13 22:14:07 +0200

    Pridané veci do hry

--- projects/imperium/src/app/play-game/left-ingame-menu/left-ingame-menu.component.ts
+++ projects/imperium/src/app/play-game/left-ingame-menu/left-ingame-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'imp-ingame-left-menu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftIngameMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  public travel(destination?: string): void {
+	switch(destination) {
+		case 'end':
+			this.router.navigate(['/welcome-screen']);
+			break;
+		default:
+			break;
+	}
+  }
 }
