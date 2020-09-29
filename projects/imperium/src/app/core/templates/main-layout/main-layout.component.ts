18262cd16f5dfaec05a1b4d65ed4b851be24ac2a
dimi3 <oresansky@gmail.com>
2020-09-29 21:40:02 +0200

    Pridané verzovanie buildov

--- projects/imperium/src/app/core/templates/main-layout/main-layout.component.ts
+++ projects/imperium/src/app/core/templates/main-layout/main-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as SettingsActions from './../../../settings/actions/settings.actions';
+import { environment } from './../../../../environments/environment';
 
 @Component({
   selector: 'imp-main-layout',
@@ -8,6 +9,7 @@ import * as SettingsActions from './../../../settings/actions/settings.actions';
   styleUrls: ['./main-layout.component.sass']
 })
 export class MainLayoutComponent implements OnInit {
+	public version: string = environment.version;
 
   constructor() { }
 
